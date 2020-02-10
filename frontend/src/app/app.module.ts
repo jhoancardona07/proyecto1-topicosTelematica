c8220a43e8865a4adc6b03a6a1bb83bd45991670
Jhoan Cardona <jscardonag@eafit.edu.co>
2020-02-09 23:34:15 -0500

    Añadiendo los archivos iniciales del frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { PrivateTasksComponent } from './components/private-tasks/private-tasks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    TasksComponent,
+    PrivateTasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
